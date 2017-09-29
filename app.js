2336697bd196ed760d3d41771b5f39f86b87e6c7
favioEvangelion <favio_evangelion@hotmail.com>
2017-09-29 12:16:19 -0400

    Calendario con jade

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 var express = require('express');
 var app = express();
-var path = require('path');
-var fs = require('fs');
 
 app.use(express.static('public'));
-app.use('/jquery', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
+app.set("view engine","jade");
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.get('/cursos', function (req, res) {
-    fs.readFile(path.join(__dirname,'cursos.json'),'utf8',function(err, cursos){
-        if(err) {
-            throw err;
-        } else {
-            res.json(JSON.parse(cursos));
-        }
-    });
+    res.render('index.jade');
 });
 
 app.listen(8080, function () {
