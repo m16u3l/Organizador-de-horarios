daeba4921819b8d267634267bbdaf18424b4fdeb
m16u3l <m16u3l_777@hotmail.com>
2017-10-01 01:55:27 -0400

    iniciando proyecto con angular

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
-app.set("view engine","jade");
+var path = require('path');
 
 app.get('/', function (req, res) {
     res.render('index.jade');
 });
 
+//iniciar html
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+//iniciar angular
+app.use('/angular', express.static(path.join(__dirname, 'node_modules', 'angular')));
+
 app.listen(8080, function () {
-    console.log("starting");
+  console.log("starting");
 });
