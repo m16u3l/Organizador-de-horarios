50f42d5e60e7e41ffd8bef14fe2eb1853c5326bc
PoHMA <miguel.poma@ceis.cs.umss.edu.bo>
2017-12-13 15:39:31 -0400

    Carga del archivo pdf y promesa a texto

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
 var app = express();
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.set("view engine","jade");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+var uploader = multer({dest: "./uploads"}); 
+var middleware_upload = uploader.single('file_input');
+
 app.use(require('./routes/principal.js'));
 app.use(require('./routes/inicio.js'));
+app.use(require('./routes/parse.js'));
 
 app.listen(8080, function () {
     console.log("starting");
